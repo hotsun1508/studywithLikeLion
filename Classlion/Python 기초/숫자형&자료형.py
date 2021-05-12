1fc7b88fa299530009a23caa758b0bf2fed27862
Sun Ah Min <sunaslight1508@gmail.com>
2021-05-12 15:29:32 +0900

    20210512

---
+++ Classlion/Python 기초/숫자형&자료형.py
@@ -0,0 +1,23 @@
+# 주석 = 메모 
+# 정수 int(integer), 실수 float
+
+num1 = int(input("First integer : "))
+num2 = int(input("Second integer : "))
+
+print(num1**num2)
+
+# 나눗셈
+    # 몫 : //
+    # 나머지 : %
+    # 몫 + 나머지 : /
+# 거듭제곱: ** 밑 ** 승수 
+    # 2**3 = 2*2*2 = 8
+
+# 문자형 자료형은 "따옴표"로 감싸주어야 함.
+# 문자를 어떻게 다뤄줄 것인가 중요 - 문자열 자체 가공하는 경우가 더 많음
+    # 인덱싱 : 0 1 2 3 4 ~ / 끝에서부터는 ~ -4 -3 -2 -1
+    # 슬라이싱 : str[index1부터:index2전까지] index1 <= 접근하고자 하는 index < index 2
+    
+
+
+
